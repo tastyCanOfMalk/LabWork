add716b231b24df8709793f136f6305992bcf323
Ed <eddytheflow@gmail.com>
2023-02-23 14:29:45 -0500

    lab12

--- my-test-app/src/app/app.module.ts
+++ my-test-app/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { VehicleFormReactiveComponent } from './vehicle-form-reactive/vehicle-form-reactive.component';
 import { HttpClientModule } from '@angular/common/http';
+import '@angular/common/locales/global/fr'
+
 
 @NgModule({
   declarations: [
